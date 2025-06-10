9d2b8d5b756eeae43de517e2a6d7a2aa21cadd2b
Víctor Ponz <victorponz@gmail.com>
2025-06-10 10:15:11 +0200

    Marcadores de electrolineras

--- src/main/java/com/electrolineras/mapa/controller/MapaController.java
+++ src/main/java/com/electrolineras/mapa/controller/MapaController.java
@@ -2,6 +2,7 @@ package com.electrolineras.mapa.controller;
 
 import com.electrolineras.mapa.service.ExternalApiService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +15,10 @@ public class MapaController {
     //https://help.opendatasoft.com/apis/ods-explore-v2/#section/Opendatasoft-Query-Language-(ODSQL)/Where-clause
     //within_distance(coordenadas, geom'POINT(-0.10582 40.61933)', 100km)
     //https://datosabiertos.dipcas.es/api/explore/v2.1/catalog/datasets/electrolineras/records?where=within_distance(coordenadas%2C%20geom%27POINT(-0.10582%2040.61933)%27%2C%20100km)&limit=20
+
+
     @GetMapping("/electrolineras")
-    public Object getData() {
-        return externalApiService.getExternalData();
+    public Object getData(@RequestParam(name="lat", required = false) String lat, @RequestParam(name="lng", required = false) String lng) {
+        return externalApiService.getExternalData(lat, lng);
     }
 }
--- src/main/java/com/electrolineras/mapa/service/ExternalApiService.java
+++ src/main/java/com/electrolineras/mapa/service/ExternalApiService.java
@@ -9,8 +9,17 @@ public class ExternalApiService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public Object getExternalData() {
-        String url = "https://datosabiertos.dipcas.es/api/explore/v2.1/catalog/datasets/electrolineras/records";  // Replace with actual URL
+    /**
+     * Hemos de comprobar el campo total_count para saber si hemos de paginar
+     * mediante sucesivas llamadas api pasándole el offest correspondiente
+     * Si, por ejemplo, total_count = 40, haremos 2 peticiones, la segunda con
+     * un offset de 20
+     * @return
+     */
+    public Object getExternalData(String lat, String lng) {
+        //String url = "https://datosabiertos.dipcas.es/api/explore/v2.1/catalog/datasets/electrolineras/records?limit=20";  // Replace with actual URL
+        String url = "https://datosabiertos.dipcas.es/api/explore/v2.1/catalog/datasets/electrolineras/records?within_distance(coordenadas,%20geom%27POINT(" + lat + "%20" + lng + ")%27,%201km)%27&limit=20";
+        System.out.println(url);
         ResponseEntity<Object> response = restTemplate.getForEntity(url, Object.class);
         return response.getBody(); // Jackson converts JSON to LinkedHashMap/List
     }
